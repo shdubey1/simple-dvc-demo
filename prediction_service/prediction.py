5ff4ebdbaf6b171d46cd15c983fbcff65bc1da12
Shubham Dubey <dubeyshubham854@gmail.com>
2022-02-03 19:55:41 +0530

    test done

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -3,20 +3,22 @@ import os
 import json
 import joblib
 import numpy as np
-import prediction_service
+
 
 params_path = "params.yaml"
 schema_path = os.path.join("prediction_service", "schema_in.json")
 
 class NotInRange(Exception):
-    def __init__(self, message="Values entered are not in range "):
+    def __init__(self, message="Values entered are not in expected range"):
         self.message = message
         super().__init__(self.message)
 
 class NotInCols(Exception):
-    def __init__(self, message="Not in Columns "):
+    def __init__(self, message="Not in cols"):
         self.message = message
         super().__init__(self.message)
+
+
 
 def read_params(config_path=params_path):
     with open(config_path) as yaml_file:
@@ -27,21 +29,20 @@ def predict(data):
     config = read_params(params_path)
     model_dir_path = config["webapp_model_dir"]
     model = joblib.load(model_dir_path)
-    prediction = model.predict(data).tolist[0]
-
+    prediction = model.predict(data).tolist()[0]
     try:
         if 3 <= prediction <= 8:
             return prediction
         else:
             raise NotInRange
     except NotInRange:
-            return "Unexpected result"
+        return "Unexpected result"
+
 
 def get_schema(schema_path=schema_path):
     with open(schema_path) as json_file:
         schema = json.load(json_file)
     return schema
-
 
 def validate_input(dict_request):
     def _validate_cols(col):
@@ -49,20 +50,19 @@ def validate_input(dict_request):
         actual_cols = schema.keys()
         if col not in actual_cols:
             raise NotInCols
-    
+
     def _validate_values(col, val):
-        schema = get_schema
-        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]):
+        schema = get_schema()
+
+        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]) :
             raise NotInRange
-
-        
-
 
     for col, val in dict_request.items():
         _validate_cols(col)
         _validate_values(col, val)
+    
+    return True
 
-        return True
 
 def form_response(dict_request):
     if validate_input(dict_request):
@@ -78,6 +78,16 @@ def api_response(dict_request):
             response = predict(data)
             response = {"response": response}
             return response
+            
+    except NotInRange as e:
+        response = {"the_exected_range": get_schema(), "response": str(e) }
+        return response
+
+    except NotInCols as e:
+        response = {"the_exected_cols": get_schema().keys(), "response": str(e) }
+        return response
+
+
     except Exception as e:
-        response={"The expected range": get_schema(), "response": str(e)}
+        response = {"response": str(e) }
         return response
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,17 @@
-import os 
-import yaml
 import pytest
+import yaml
+import os
 import json
+
 
 @pytest.fixture
 def config(config_path="params.yaml"):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
-        return config
+    return config
 
 @pytest.fixture
 def schema_in(schema_path="schema_in.json"):
     with open(schema_path) as json_file:
         schema = json.load(json_file)
-        return schema
+    return schema
