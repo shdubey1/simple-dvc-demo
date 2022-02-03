ce5e66d1d9e1c0f007ad1190bec3ab93b41ad31a
Shubham Dubey <dubeyshubham854@gmail.com>
2022-02-03 19:41:21 +0530

    test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,4 @@
 # for test case purpose
-import imp
-import pytest
-import json
-import joblib
-import logging
-import os
-from prediction_service.prediction import form_response, api_response
-import prediction_service
-
-
 import json
 import logging
 import os
@@ -73,3 +63,15 @@ def test_form_response_correct_range(data=input_data["correct_range"]):
 def test_api_response_correct_range(data=input_data["correct_range"]):
     res = api_response(data)
     assert  TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
+
+def test_form_response_incorrect_range(data=input_data["incorrect_range"]):
+    with pytest.raises(prediction_service.prediction.NotInRange):
+        res = form_response(data)
+
+def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInRange().message
+
+def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInCols().message
